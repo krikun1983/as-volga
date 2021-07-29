ab370f50d6a15579f8de4c289767ccaba14df967
@krikun1983 <akhmadeevst@gmail.com>
2021-07-29 11:09:57 +0400

    feat: add variables colors device-width

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Header from './components/header';
 
-const App = (): JSX.Element => <div>Hello</div>;
+const App = (): JSX.Element => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
 
 ReactDOM.render(<App />, document.getElementById('root'));
