606f728755125e7233c29972788e1bed34068f7c
@krikun1983 <akhmadeevst@gmail.com>
2021-07-29 14:46:52 +0400

    feat: add fonts

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -7,7 +7,16 @@ const Header = (): JSX.Element => {
     <header className="header">
       <div className="container flex-space">
         <Logo />
-        <Nav />
+        <div className="header__links flex-column">
+          <div className="header__top flex-space-end">
+            <h1 className="header__heading">Издательство «АртСервис-Волга»</h1>
+            <div className="header__contact">
+              <div>Тел. 8(845-2)25-36-08</div>
+              <div>E-mail: as-volga@mail.ru</div>
+            </div>
+          </div>
+          <Nav />
+        </div>
       </div>
     </header>
   );
